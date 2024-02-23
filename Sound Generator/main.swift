c839c6bee0cea44fda7613d51fcc2a57df850262
Kasper Nielsen <kasper0406@gmail.com>
2024-02-23 18:03:53 +0100

    Output progress and split larger datasets

--- Sound Generator/main.swift
+++ Sound Generator/main.swift
@@ -33,26 +33,47 @@ let instruments = [
         sampleName: "candp"
     )
 ]
+let samplesPerInstrument = 50000
+let totalSamples = instruments.count * samplesPerInstrument
 
 let renderer = try SampleRenderer()
 let generator = EventGenerator()
 
+func updateLine(with newText: String) {
+    let clearLineSequence = "\r" + String(repeating: " ", count: 80) + "\r"
+    print(clearLineSequence, terminator: "")
+    print(newText, terminator: "")
+    fflush(stdout) // Ensure the output is immediately displayed
+}
+
+updateLine(with: "Generating samples...")
+var count = 0
 for instrument in instruments {
     try renderer.useInstrument(instrumentPack: instrument.url)
 
-    for i in 0..<10000 {
+    for i in 0..<samplesPerInstrument {
         renderer.clearTracks()
 
         for midiEvent in generator.generate(instrumentSpec: instrument) {
             renderer.stage(note: midiEvent)
         }
 
-        let fileName = "\(instrument.category)_\(instrument.sampleName)_\(i)"
-        let aacOutputFile = datasetDirectory.appending(path: "\(fileName).aac")
-        let csvOutputFile = datasetDirectory.appending(path: "\(fileName).csv")
+        let instrumentName = "\(instrument.category)_\(instrument.sampleName)"
+        let fileName = "\(instrumentName)_\(i)"
+        // Maximum of 5000 files in one directory
+        let datasetPartition = datasetDirectory.appending(path: "\(instrumentName)_\(i / 5000)")
+        if !FileManager.default.fileExists(atPath: datasetPartition.path) {
+            try FileManager.default.createDirectory(at: datasetPartition, withIntermediateDirectories: false)
+        }
+        let aacOutputFile = datasetPartition.appending(path: "\(fileName).aac")
+        let csvOutputFile = datasetPartition.appending(path: "\(fileName).csv")
 
         let csvString = noteEventsToCsv(notes: try renderer.getStagedEvents())
         try csvString.write(to: csvOutputFile, atomically: false, encoding: .utf8)
         try renderer.generateAac(outputUrl: aacOutputFile)
+
+        count += 1
+        let completionPercent = (Double(count) / Double(totalSamples)) * 100
+        updateLine(with: "\(String(format: "%.3f", completionPercent))% complete")
     }
 }
